41584562ceb1c24a61610d9fa81241431dcf531b
Roland van Ipenburg <roland.vanipenburg@puurpxl.nl>
2013-01-20 00:37:40 +0100

    copied widget code

---
+++ android/assets/www/en.lproj/scripts/strings.js
@@ -0,0 +1,5 @@
+var localizedStrings = new Array;
+localizedStrings['Done'] = 'Done';
+localizedStrings['Widget'] = 'Widget';
+localizedStrings['Concept &amp; data by'] = 'Concept &amp; data by';
+localizedStrings['Images by'] = 'Images by';
